3a5d3584d4c88df38985cc60f45df3988850b9e7
Michael Hilton <michael.hilton@gmail.com>
2013-10-04 15:12:03 -0700

    first test

--- src/edu/oregonstate/cope/clientRecorder/ClientRecorder.java
+++ src/edu/oregonstate/cope/clientRecorder/ClientRecorder.java
@@ -2,10 +2,13 @@ package edu.oregonstate.cope.clientRecorder;
 
 /**
  * Created with IntelliJ IDEA.
- * User: michaelhilton
+ * User: michael.hilton
  * Date: 10/4/13
  *
  * To change this template use File | Settings | File Templates.
  */
 public class ClientRecorder {
+     public void recordTextChange(){
+
+     }
 }
--- tests/edu/oregonstate/cope/clientRecorder/ClientRecorderTest.java
+++ tests/edu/oregonstate/cope/clientRecorder/ClientRecorderTest.java
@@ -4,8 +4,12 @@ package edu.oregonstate.cope.clientRecorder;
  * Created with IntelliJ IDEA.
  * User: michaelhilton
  * Date: 10/4/13
- *
+ * Time: 3:08 PM
  * To change this template use File | Settings | File Templates.
  */
 public class ClientRecorderTest {
+    @org.junit.Test
+    public void testRecordTextChange() throws Exception {
+
+    }
 }
