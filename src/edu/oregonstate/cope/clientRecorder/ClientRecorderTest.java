ce2ccc1ac75d557c4a456656e1751e49f4ef14d3
Michael Hilton <michael.hilton@gmail.com>
2013-10-04 14:40:33 -0700

    added package

---
+++ src/edu/oregonstate/cope/clientRecorder/ClientRecorderTest.java
@@ -0,0 +1,11 @@
+package edu.oregonstate.cope.clientRecorder;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: michaelhilton
+ * Date: 10/4/13
+ * Time: 2:39 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ClientRecorderTest {
+}
