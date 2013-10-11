bb1f74a1f01fa3459b0a88eb71d972d74c307574
Caius Brindescu <brind@illinois.edu>
2013-10-11 15:52:26 -0700

    Ignoring saves generated by the AST rewriting during refactorings

--- edu.oregonstate.cope.eclipse/src/edu/oregonstate/cope/eclipse/listeners/ResourceListener.java
+++ edu.oregonstate.cope.eclipse/src/edu/oregonstate/cope/eclipse/listeners/ResourceListener.java
@@ -7,7 +7,7 @@ public class ResourceListener implements IResourceChangeListener {
 
 	@Override
 	public void resourceChanged(IResourceChangeEvent event) {
-		if (isSavedAction())
+		if (isSavedAction() || isRefactoringInProgress())
 			System.out.println("This is a save action, so I will ignore it");
 		else
 			System.out.println("This is a genuine refresh");
@@ -16,5 +16,9 @@ public class ResourceListener implements IResourceChangeListener {
 	private boolean isSavedAction() {
 		long currentTime = System.currentTimeMillis();
 		return currentTime - SaveCommandExecutionListener.getLastSaveAction() < 500;
+	}
+	
+	private boolean isRefactoringInProgress() {
+		return RefactoringExecutionListener.isRefactoringInProgress();
 	}
 }
