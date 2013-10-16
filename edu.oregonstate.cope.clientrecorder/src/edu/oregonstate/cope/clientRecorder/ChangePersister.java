d8c7997030f3c5ca2c5466e5d3558c42854244c6
cdmihai <codobanmihai@gmail.com>
2013-10-15 18:16:25 -0700

    first try at persist method. It does not work because it appends to file, it does not rewrite

--- edu.oregonstate.cope.clientrecorder/src/edu/oregonstate/cope/clientRecorder/ChangePersister.java
+++ edu.oregonstate.cope.clientrecorder/src/edu/oregonstate/cope/clientRecorder/ChangePersister.java
@@ -2,11 +2,14 @@ package edu.oregonstate.cope.clientRecorder;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+
+import javax.management.RuntimeErrorException;
 
 /**
  * Persists JSON objects to disc. This class is a Singleton.
@@ -23,8 +26,7 @@ public class ChangePersister {
 		try {
 			writer = new BufferedWriter(new FileWriter("testFileWrite.txt"));
 		} catch (IOException e) {
-			e.printStackTrace(); // To change body of catch statement use File |
-									// Settings | File Templates.
+			e.printStackTrace();
 		}
 		init();
 	}
@@ -37,8 +39,7 @@ public class ChangePersister {
 		try {
 			writer.write(jsonArr.toJSONString());
 		} catch (IOException e) {
-			e.printStackTrace(); // To change body of catch statement use File |
-									// Settings | File Templates.
+			e.printStackTrace();
 		}
 	}
 
@@ -53,7 +54,19 @@ public class ChangePersister {
 	}
 
 	public void persist(JSONObject jsonObject) {
-
+		if(jsonObject == null){
+			throw new RuntimeException("Argument cannot be null");
+		}
+		
+		JSONArray persistedContent = (JSONArray) JSONValue.parse(writer.toString());
+		persistedContent.add(jsonObject);
+		
+		try {
+			writer.write(persistedContent.toJSONString());
+			writer.flush();
+		} catch (IOException e) {
+			throw new RuntimeException("could not write JSON to file");
+		}
 	}
 
 	protected void setWriter(Writer stringWriter) {
--- edu.oregonstate.cope.clientrecorder/tests/edu/oregonstate/cope/clientRecorder/ChangePersisterTest.java
+++ edu.oregonstate.cope.clientrecorder/tests/edu/oregonstate/cope/clientRecorder/ChangePersisterTest.java
@@ -2,11 +2,13 @@ package edu.oregonstate.cope.clientRecorder;
 
 import org.json.simple.*;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.StringWriter;
 
 import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class ChangePersisterTest {
 
@@ -17,6 +19,10 @@ public class ChangePersisterTest {
 		stringWriter = new StringWriter();
 		ChangePersister.instance().setWriter(stringWriter);
 		ChangePersister.instance().init();
+		testInit();
+	}
+
+	private void testInit() {
 		JSONArray jarr = getJsonArray();
 		assertEquals(jarr.size(), 1);
 		assertEquals(((JSONObject) jarr.get(0)).get("eventType"), "FileInit");
@@ -25,15 +31,30 @@ public class ChangePersisterTest {
 	private JSONArray getJsonArray() {
 		return (JSONArray) JSONValue.parse(stringWriter.toString());
 	}
-
+	
+	@Test
+	public void testPersistNull() throws Exception {
+		RuntimeException caughtException = null;
+		
+		try{
+			ChangePersister.instance().persist(null);
+		}
+		catch(RuntimeException exception){
+			caughtException = exception;
+		}
+		assertEquals(caughtException.getClass(), RuntimeException.class);
+		testInit();
+	}
+	
 	@Test
 	public void testPersistRecord() {
 		JSONObject objToRecord = new JSONObject();
 		objToRecord.put("test", "fileIO");
+		
 		ChangePersister.instance().persist(objToRecord);
+		
 		JSONArray jarr = getJsonArray();
 		assertEquals(jarr.size(), 2);
 		assertEquals(((JSONObject) jarr.get(1)).get("test"), "FileIO");
 	}
-
 }
