963c42baf84e93675b7794d71ab421c2b93abc0a
Mihai Codoban <codobanm@glennallen.eecs.oregonstate.edu>
2013-10-15 17:37:26 -0700

    formatting

--- edu.oregonstate.cope.clientrecorder/src/edu/oregonstate/cope/clientRecorder/ChangePersister.java
+++ edu.oregonstate.cope.clientrecorder/src/edu/oregonstate/cope/clientRecorder/ChangePersister.java
@@ -13,36 +13,38 @@ import java.io.Writer;
  */
 public class ChangePersister {
 
-    private Writer writer;
+	private Writer writer;
 
 	private static class Instance {
 		public static final ChangePersister instance = new ChangePersister();
 	}
 
 	private ChangePersister() {
-        try {
-            writer = new BufferedWriter(new FileWriter("testFileWrite.txt"));
-        } catch (IOException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-        init();
+		try {
+			writer = new BufferedWriter(new FileWriter("testFileWrite.txt"));
+		} catch (IOException e) {
+			e.printStackTrace(); // To change body of catch statement use File |
+									// Settings | File Templates.
+		}
+		init();
 	}
 
 	public void init() {
-        JSONArray jsonArr = new JSONArray();
-        JSONObject markerObject = createInitJSON();
-        jsonArr.add(markerObject);
-        
-        try {
-            writer.write(jsonArr.toJSONString());
-        } catch (IOException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-    }
+		JSONArray jsonArr = new JSONArray();
+		JSONObject markerObject = createInitJSON();
+		jsonArr.add(markerObject);
+
+		try {
+			writer.write(jsonArr.toJSONString());
+		} catch (IOException e) {
+			e.printStackTrace(); // To change body of catch statement use File |
+									// Settings | File Templates.
+		}
+	}
 
 	private JSONObject createInitJSON() {
 		JSONObject markerObject = new JSONObject();
-        markerObject.put("eventType", "FileInit");
+		markerObject.put("eventType", "FileInit");
 		return markerObject;
 	}
 
@@ -51,7 +53,7 @@ public class ChangePersister {
 	}
 
 	public void persist(JSONObject jsonObject) {
-		
+
 	}
 
 	protected void setWriter(Writer stringWriter) {
--- edu.oregonstate.cope.clientrecorder/tests/edu/oregonstate/cope/clientRecorder/ChangePersisterTest.java
+++ edu.oregonstate.cope.clientrecorder/tests/edu/oregonstate/cope/clientRecorder/ChangePersisterTest.java
@@ -9,33 +9,31 @@ import java.io.StringWriter;
 import static junit.framework.Assert.assertEquals;
 
 public class ChangePersisterTest {
-	
+
 	private StringWriter stringWriter;
 
 	@Before
 	public void setup() {
 		stringWriter = new StringWriter();
 		ChangePersister.instance().setWriter(stringWriter);
-        ChangePersister.instance().init();
-        JSONArray jarr = getJsonArray();
-        assertEquals(jarr.size(),1);
-        assertEquals(((JSONObject) jarr.get(0)).get("eventType"), "FileInit");
-    }
+		ChangePersister.instance().init();
+		JSONArray jarr = getJsonArray();
+		assertEquals(jarr.size(), 1);
+		assertEquals(((JSONObject) jarr.get(0)).get("eventType"), "FileInit");
+	}
 
-    private JSONArray getJsonArray() {
-        return (JSONArray) JSONValue.parse(stringWriter.toString());
-    }
+	private JSONArray getJsonArray() {
+		return (JSONArray) JSONValue.parse(stringWriter.toString());
+	}
 
-    @Test
+	@Test
 	public void testPersistRecord() {
-        JSONObject objToRecord = new JSONObject();
-        objToRecord.put("test","fileIO");
-        ChangePersister.instance().persist(objToRecord);
-        JSONArray jarr = getJsonArray();
-        assertEquals(jarr.size(),2);
-        assertEquals(((JSONObject) jarr.get(1)).get("test"), "FileIO");
-    }
-
-
+		JSONObject objToRecord = new JSONObject();
+		objToRecord.put("test", "fileIO");
+		ChangePersister.instance().persist(objToRecord);
+		JSONArray jarr = getJsonArray();
+		assertEquals(jarr.size(), 2);
+		assertEquals(((JSONObject) jarr.get(1)).get("test"), "FileIO");
+	}
 
 }
