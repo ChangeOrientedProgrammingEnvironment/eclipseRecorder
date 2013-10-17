33508acabdd8dac3b9362c2e9180d7e54e04db2b
cdmihai <codobanmihai@gmail.com>
2013-10-16 18:40:42 -0700

    shortened separator

--- edu.oregonstate.cope.clientrecorder/tests/edu/oregonstate/cope/clientRecorder/ChangePersisterTest.java
+++ edu.oregonstate.cope.clientrecorder/tests/edu/oregonstate/cope/clientRecorder/ChangePersisterTest.java
@@ -1,14 +1,19 @@
 package edu.oregonstate.cope.clientRecorder;
 
-import org.json.simple.*;
+import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.*;
+
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import java.io.StringWriter;
-
-import static junit.framework.Assert.assertEquals;
-import static org.junit.Assert.*;
 
 public class ChangePersisterTest {
 
@@ -22,16 +27,30 @@ public class ChangePersisterTest {
 		testInit();
 	}
 
-	private void testInit() {
-		JSONArray jarr = getJsonArray();
+	@Test
+	public void testInit() {
+		List<JSONObject> jarr = getJsonArray();
 		assertEquals(jarr.size(), 1);
-		assertEquals(((JSONObject) jarr.get(0)).get("eventType"), "FileInit");
+		assertEquals(jarr.get(0).get("eventType"), "FileInit");
 	}
 
-	private JSONArray getJsonArray() {
-		return (JSONArray) JSONValue.parse(stringWriter.toString());
+	private List<JSONObject> getJsonArray() {
+		// return (JSONArray) JSONValue.parse(stringWriter.toString());
+		List<String> allMatches = new ArrayList<>();
+		Matcher m = ChangePersister.ELEMENT_REGEX.matcher(stringWriter.toString());
+		while (m.find()) {
+			allMatches.add(m.group(1));
+		}
+		
+		List<JSONObject> jsonEvents = new ArrayList<>();
+		for (String jsonString : allMatches) {
+			JSONObject obj = (JSONObject) JSONValue.parse(jsonString);
+			jsonEvents.add(obj);
+		}
+		
+		return jsonEvents;
 	}
-
+	
 	@Test
 	public void testPersistNull() throws Exception {
 		RuntimeException caughtException = null;
@@ -52,8 +71,8 @@ public class ChangePersisterTest {
 
 		ChangePersister.instance().persist(objToRecord);
 
-		JSONArray jarr = getJsonArray();
+		List<JSONObject> jarr = getJsonArray();
 		assertEquals(jarr.size(), 2);
-		assertEquals(((JSONObject) jarr.get(1)).get("test"), "FileIO");
+		assertEquals(jarr.get(1).get("test"), "fileIO");
 	}
 }
