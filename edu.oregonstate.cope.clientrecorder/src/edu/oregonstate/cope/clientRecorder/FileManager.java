e27aba8423c287e7da399526b88f7823b04ccdec
cdmihai <codo@illinois.edu>
2013-10-24 14:13:42 -0700

    fixed bug when computing the event file names

--- edu.oregonstate.cope.clientrecorder/src/edu/oregonstate/cope/clientRecorder/FileManager.java
+++ edu.oregonstate.cope.clientrecorder/src/edu/oregonstate/cope/clientRecorder/FileManager.java
@@ -32,7 +32,7 @@ public class FileManager {
 
 	protected String getFileName() {
 		Calendar cal = Calendar.getInstance();
-		String pathName = cal.get(Calendar.MONTH) + "-" + cal.get(Calendar.MONTH) + "-" + cal.get(Calendar.DAY_OF_MONTH);
+		String pathName = cal.get(Calendar.YEAR) + "-" + cal.get(Calendar.MONTH) + "-" + cal.get(Calendar.DAY_OF_MONTH);
 		
 		return pathName;
 	}
--- edu.oregonstate.cope.clientrecorder/tests/edu/oregonstate/cope/clientRecorder/FileManagerTest.java
+++ edu.oregonstate.cope.clientrecorder/tests/edu/oregonstate/cope/clientRecorder/FileManagerTest.java
@@ -39,9 +39,9 @@ public class FileManagerTest {
 	@Test
 	public void testFileName() throws Exception {
 		Calendar cal = Calendar.getInstance();
-		String expected = cal.get(Calendar.MONTH) + "-" + cal.get(Calendar.MONTH) + "-" + cal.get(Calendar.DAY_OF_MONTH);
+		String expected = cal.get(Calendar.YEAR) + "-" + cal.get(Calendar.MONTH) + "-" + cal.get(Calendar.DAY_OF_MONTH);
 
-		assertEquals(fm.getFileName(), expected);
+		assertEquals(expected, fm.getFileName());
 	}
 
 	@Test
