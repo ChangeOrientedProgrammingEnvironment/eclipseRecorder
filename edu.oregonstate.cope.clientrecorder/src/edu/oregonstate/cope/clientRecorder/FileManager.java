a4730c391cc1efb328f0c538fe8b906f70bcfe37
cdmihai <codo@illinois.edu>
2013-10-25 10:30:05 -0700

    reordered methods

--- edu.oregonstate.cope.clientrecorder/src/edu/oregonstate/cope/clientRecorder/FileManager.java
+++ edu.oregonstate.cope.clientrecorder/src/edu/oregonstate/cope/clientRecorder/FileManager.java
@@ -11,34 +11,8 @@ public class FileManager {
 
 	private String rootDirectory = "outputFiles";
 
-	//TODO do not change state and also return
-	public Path getFilePath() throws IOException {
-		Path filePath = Paths.get(rootDirectory, getFileName());
-		
-		if (!Files.exists(filePath)) {
-			Files.createFile(filePath);
-		}
-
-		return filePath;
-	}
-	
 	public void setRootDirectory(String rootDirectory){
 		this.rootDirectory = rootDirectory;
-	}
-
-	public void deleteEventFiles() throws IOException{
-		DirectoryStream<Path> directoryStream = Files.newDirectoryStream(Paths.get(rootDirectory));
-		
-		for (Path path : directoryStream) {
-			Files.delete(path);
-		}
-	}
-
-	protected String getFileName() {
-		Calendar cal = Calendar.getInstance();
-		String pathName = cal.get(Calendar.YEAR) + "-" + cal.get(Calendar.MONTH) + "-" + cal.get(Calendar.DAY_OF_MONTH);
-		
-		return pathName;
 	}
 
 	public void write(String string) {
@@ -56,8 +30,34 @@ public class FileManager {
 		return false; 
 	}
 
+	public void deleteEventFiles() throws IOException{
+		DirectoryStream<Path> directoryStream = Files.newDirectoryStream(Paths.get(rootDirectory));
+		
+		for (Path path : directoryStream) {
+			Files.delete(path);
+		}
+	}
+
 	public void commitChanges() {
 		// TODO Auto-generated method stub
 		
+	}
+
+	//TODO do not change state and also return
+	protected Path getFilePath() throws IOException {
+		Path filePath = Paths.get(rootDirectory, getFileName());
+		
+		if (!Files.exists(filePath)) {
+			Files.createFile(filePath);
+		}
+	
+		return filePath;
+	}
+
+	protected String getFileName() {
+		Calendar cal = Calendar.getInstance();
+		String pathName = cal.get(Calendar.YEAR) + "-" + cal.get(Calendar.MONTH) + "-" + cal.get(Calendar.DAY_OF_MONTH);
+		
+		return pathName;
 	}
 }
--- edu.oregonstate.cope.clientrecorder/tests/edu/oregonstate/cope/clientRecorder/FileManagerTest.java
+++ edu.oregonstate.cope.clientrecorder/tests/edu/oregonstate/cope/clientRecorder/FileManagerTest.java
@@ -37,7 +37,7 @@ public class FileManagerTest {
 	}
 
 	@Test
-	public void testFileName() throws Exception {
+	public void testFileName() {
 		Calendar cal = Calendar.getInstance();
 		String expected = cal.get(Calendar.YEAR) + "-" + cal.get(Calendar.MONTH) + "-" + cal.get(Calendar.DAY_OF_MONTH);
 
