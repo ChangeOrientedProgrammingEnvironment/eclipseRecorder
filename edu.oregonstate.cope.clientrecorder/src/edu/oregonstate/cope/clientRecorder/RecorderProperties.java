afa76d3028bdd6feaa8f9610b8c2ac2a5a3b1486
cdmihai <codobanmihai@gmail.com>
2013-10-29 17:02:09 -0400

    Added the basic functionality for the Properties features.

--- edu.oregonstate.cope.clientrecorder/src/edu/oregonstate/cope/clientRecorder/RecorderProperties.java
+++ edu.oregonstate.cope.clientrecorder/src/edu/oregonstate/cope/clientRecorder/RecorderProperties.java
@@ -1,5 +1,60 @@
 package edu.oregonstate.cope.clientRecorder;
 
+import java.util.HashMap;
+import java.util.List;
+
+import edu.oregonstate.cope.clientRecorder.fileOps.FileProvider;
+
 public class RecorderProperties {
 
+	private FileProvider fileProvider;
+	private HashMap<String, String> properties = new HashMap<>();
+
+	public RecorderProperties(FileProvider fileProvider) {
+		this.fileProvider = fileProvider;
+		initFromProvider(fileProvider);
+	}
+
+	private void initFromProvider(FileProvider fileProvider) {
+		List<String> lines = fileProvider.readAllLines(); 
+
+		for (String line : lines) {
+			int separatorIndex = line.indexOf("=");
+			String key = line.substring(0, separatorIndex);
+			String value = line.substring(separatorIndex + 1);
+
+			properties.put(key, value);
+		}
+	}
+
+	public void addProperty(String key, String value) {
+		if(key == null)
+			return;
+		
+		if(key.isEmpty() && value.isEmpty())
+			return;
+		
+		properties.put(key, value);
+		persist();
+	}
+
+	private void persist() {
+		StringBuffer sb = new StringBuffer();
+
+		for (String key : properties.keySet()) {
+			sb.append(key + "=" + properties.get(key) + "\n");
+		}
+
+		fileProvider.writeToCurrentFile(sb.toString());
+	}
+
+	public String getProperty(String key) {
+		if (key == null)
+			return null;
+		
+		if(key.isEmpty() && properties.get(key).isEmpty())
+			return "";
+		
+		return properties.get(key);
+	}
 }
--- edu.oregonstate.cope.clientrecorder/tests/edu/oregonstate/cope/clientRecorder/RecorderPropertiesTest.java
+++ edu.oregonstate.cope.clientrecorder/tests/edu/oregonstate/cope/clientRecorder/RecorderPropertiesTest.java
@@ -2,13 +2,39 @@ package edu.oregonstate.cope.clientRecorder;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import edu.oregonstate.cope.tests.util.StubFileProvider;
 
 public class RecorderPropertiesTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	private RecorderProperties properties;
+	private StubFileProvider fileProvider;
+
+	@Before
+	public void setup() {
+		fileProvider = new StubFileProvider();
+		properties = new RecorderProperties(fileProvider);
 	}
 
+	@Test
+	public void testAddNull() {
+		properties.addProperty(null, null);
+		assertNull(properties.getProperty(null), null);
+		assertTrue(fileProvider.isCurrentFileEmpty());
+	}
+	
+	@Test
+	public void testAddEmpty() throws Exception {
+		properties.addProperty("", "");
+		assertTrue(fileProvider.isCurrentFileEmpty());
+	}
+	
+	@Test
+	public void testAddNonEmpty() throws Exception {
+		properties.addProperty("k", "v");
+		assertEquals("v", properties.getProperty("k"));
+		assertEquals("v", new RecorderProperties(fileProvider).getProperty("k"));
+	}
 }
