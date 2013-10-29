09c9ba0fa9634567bfd07d23825a8915a79c5661
Caius Brindescu <brind@illinois.edu>
2013-10-28 17:19:20 -0700

    Recording the change that happened during a refresh

--- edu.oregonstate.cope.eclipse/src/edu/oregonstate/cope/eclipse/listeners/ResourceListener.java
+++ edu.oregonstate.cope.eclipse/src/edu/oregonstate/cope/eclipse/listeners/ResourceListener.java
@@ -1,16 +1,20 @@
 package edu.oregonstate.cope.eclipse.listeners;
 
+import java.io.InputStream;
+import java.util.Scanner;
+
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceChangeEvent;
 import org.eclipse.core.resources.IResourceChangeListener;
 import org.eclipse.core.resources.IResourceDelta;
+import org.eclipse.core.runtime.CoreException;
 
 import edu.oregonstate.cope.clientRecorder.ClientRecorder;
 import edu.oregonstate.cope.eclipse.COPEPlugin;
 
 public class ResourceListener implements IResourceChangeListener {
-	
+
 	ClientRecorder recorder = COPEPlugin.getDefault().getClientRecorderInstance();
 
 	@Override
@@ -22,19 +26,34 @@ public class ResourceListener implements IResourceChangeListener {
 	}
 
 	private void recordRefresh(IResourceDelta delta) {
+		if (delta == null)
+			return ;
+		
 		IResource affectedResource = delta.getResource();
 		int type = affectedResource.getType();
 		if (type == IResource.FILE) {
-			// do record change
+			IFile affectedFile = (IFile) affectedResource;
+			InputStream inputStream;
+			try {
+				inputStream = affectedFile.getContents();
+				Scanner scanner = new Scanner(inputStream, affectedFile.getCharset());
+				String contents = scanner.useDelimiter("\\A").next();	
+				scanner.close();
+				recorder.recordTextChange(contents, 0, 0, affectedFile.getFullPath().toPortableString(), ClientRecorder.REFRESH_CHANGE);
+			} catch (CoreException e) {
+			}
 		}
 		IResourceDelta[] children = delta.getAffectedChildren();
+		for (IResourceDelta child : children) {
+			recordRefresh(child);
+		}
 	}
 
 	private boolean isSavedAction() {
 		long currentTime = System.currentTimeMillis();
 		return currentTime - SaveCommandExecutionListener.getLastSaveAction() < 500;
 	}
-	
+
 	private boolean isRefactoringInProgress() {
 		return RefactoringExecutionListener.isRefactoringInProgress();
 	}
