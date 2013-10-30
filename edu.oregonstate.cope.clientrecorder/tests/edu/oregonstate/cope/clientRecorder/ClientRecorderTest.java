d62ce30df5ef5aea2360c88bccd736f87dfd62f6
cdmihai <codobanmihai@gmail.com>
2013-10-30 18:44:08 -0400

    Added timestamp to events.
    Updated tests with new method that checks whether two JSON objects are the same. It currently ignores the "timestamp field"

--- edu.oregonstate.cope.clientrecorder/tests/edu/oregonstate/cope/clientRecorder/ClientRecorderTest.java
+++ edu.oregonstate.cope.clientrecorder/tests/edu/oregonstate/cope/clientRecorder/ClientRecorderTest.java
@@ -9,57 +9,56 @@ import static org.junit.Assert.*;
 
 public class ClientRecorderTest {
 
+	private ClientRecorder clientRecorder;
 
-    private ClientRecorder clientRecorder;
-
-    @Before
-    public void setup(){
-        clientRecorder = new ClientRecorder();
-        clientRecorder.setIDE("IDEA");
-    }
+	@Before
+	public void setup() {
+		clientRecorder = new ClientRecorder();
+		clientRecorder.setIDE("IDEA");
+	}
 
 	/* Text Change Tests */
 	@Test(expected = RuntimeException.class)
 	public void testRecordTextChangeNull() throws Exception {
-        clientRecorder.buildTextChangeJSON(null, 0, 0, null, null);
+		clientRecorder.buildTextChangeJSON(null, 0, 0, null, null);
 	}
 
 	@Test(expected = RuntimeException.class)
 	public void testRecordTextChangeNoSourceFile() throws Exception {
-        clientRecorder.buildTextChangeJSON("", 0, 0, "", "");
+		clientRecorder.buildTextChangeJSON("", 0, 0, "", "");
 	}
 
 	@Test(expected = RuntimeException.class)
 	public void testRecordTextChangeNoOrigin() throws Exception {
-        clientRecorder.buildTextChangeJSON("", 0, 0, "/sampleFile", "");
+		clientRecorder.buildTextChangeJSON("", 0, 0, "/sampleFile", "");
 	}
 
 	@Test
 	public void testRecordTextChangeNoOp() throws Exception {
 		JSONObject result1 = clientRecorder.buildTextChangeJSON("", 0, 0, "/sampleFile", "changeOrigin");
 		JSONObject obj = createChangeJSON("", 0, 0, "/sampleFile", "changeOrigin");
-		assertEquals(result1, obj);
+		assertJSONEquals(result1, obj);
 	}
 
 	@Test
 	public void testRecordTextChangeAdd() throws Exception {
 		JSONObject result1 = clientRecorder.buildTextChangeJSON("addedText", 0, 0, "/sampleFile", "changeOrigin");
 		JSONObject obj = createChangeJSON("addedText", 0, 0, "/sampleFile", "changeOrigin");
-		assertEquals(result1, obj);
+		assertJSONEquals(result1, obj);
 	}
 
 	@Test
 	public void testRecordTextChangeDelete() throws Exception {
 		JSONObject result1 = clientRecorder.buildTextChangeJSON("", 0, 0, "/sampleFile", "changeOrigin");
 		JSONObject obj = createChangeJSON("", 0, 0, "/sampleFile", "changeOrigin");
-		assertEquals(result1, obj);
+		assertJSONEquals(result1, obj);
 	}
 
 	@Test
 	public void testRecordTextChangeReplace() throws Exception {
 		JSONObject result1 = clientRecorder.buildTextChangeJSON("addedText", 3, 0, "/sampleFile", "changeOrigin");
 		JSONObject obj = createChangeJSON("addedText", 3, 0, "/sampleFile", "changeOrigin");
-		assertEquals(result1, obj);
+		assertJSONEquals(result1, obj);
 	}
 
 	private JSONObject createChangeJSON(String text, int offset, int length, String sourceFile, String changeOrigin) {
@@ -71,75 +70,98 @@ public class ClientRecorderTest {
 		j.put("sourceFile", sourceFile);
 		j.put("changeOrigin", changeOrigin);
 		j.put("IDE", clientRecorder.getIDE());
+		j.put("timestamp", System.currentTimeMillis() / 1000);
 		return j;
 	}
 
 	/* Test DebugLaunch */
 	@Test(expected = RuntimeException.class)
 	public void testDebugLaunchNull() throws Exception {
-        clientRecorder.buildIDEFileEventJSON(null, null);
+		clientRecorder.buildIDEFileEventJSON(null, null);
 	}
 
-    @Test
-    public void testDebugLaunch() throws Exception {
-        JSONObject retObj = clientRecorder.buildIDEFileEventJSON(ClientRecorder.EventType.debugLaunch, "/workspace/package/filename.java");
-        JSONObject expected = new JSONObject();
-        expected.put("IDE","IDEA");
-        expected.put("eventType",ClientRecorder.EventType.debugLaunch.toString());
-        expected.put("fullyQualifiedMain","/workspace/package/filename.java") ;
-        assertEquals(expected, retObj);
-    }
+	@Test
+	public void testDebugLaunch() throws Exception {
+		JSONObject retObj = clientRecorder.buildIDEFileEventJSON(ClientRecorder.EventType.debugLaunch, "/workspace/package/filename.java");
+		JSONObject expected = new JSONObject();
+		expected.put("IDE", "IDEA");
+		expected.put("eventType", ClientRecorder.EventType.debugLaunch.toString());
+		expected.put("fullyQualifiedMain", "/workspace/package/filename.java");
+		expected.put("timestamp", System.currentTimeMillis() / 1000);
 
-    @Test
-    public void testStdLaunch() throws Exception {
-        JSONObject retObj = clientRecorder.buildIDEFileEventJSON(ClientRecorder.EventType.normalLaunch, "/workspace/package/filename.java");
-        JSONObject expected = new JSONObject();
-        expected.put("IDE","IDEA");
-        expected.put("eventType",ClientRecorder.EventType.normalLaunch.toString());
-        expected.put("fullyQualifiedMain","/workspace/package/filename.java") ;
-        assertEquals(expected, retObj);
-    }
+		assertJSONEquals(expected, retObj);
+	}
 
-    @Test
-    public void testFileOpen() throws Exception {
-        JSONObject retObj = clientRecorder.buildIDEFileEventJSON(ClientRecorder.EventType.fileOpen, "/workspace/package/filename.java");
-        JSONObject expected = new JSONObject();
-        expected.put("IDE","IDEA");
-        expected.put("eventType",ClientRecorder.EventType.fileOpen.toString());
-        expected.put("fullyQualifiedMain","/workspace/package/filename.java") ;
-        assertEquals(expected, retObj);
-    }
+	@Test
+	public void testStdLaunch() throws Exception {
+		JSONObject retObj = clientRecorder.buildIDEFileEventJSON(ClientRecorder.EventType.normalLaunch, "/workspace/package/filename.java");
+		JSONObject expected = new JSONObject();
+		expected.put("IDE", "IDEA");
+		expected.put("eventType", ClientRecorder.EventType.normalLaunch.toString());
+		expected.put("fullyQualifiedMain", "/workspace/package/filename.java");
+		expected.put("timestamp", System.currentTimeMillis() / 1000);
 
-    @Test
-    public void testFileClose() throws Exception {
-        JSONObject retObj = clientRecorder.buildIDEFileEventJSON(ClientRecorder.EventType.fileClose, "/workspace/package/filename.java");
-        JSONObject expected = new JSONObject();
-        expected.put("IDE","IDEA");
-        expected.put("eventType",ClientRecorder.EventType.fileClose.toString());
-        expected.put("fullyQualifiedMain","/workspace/package/filename.java") ;
-        assertEquals(expected, retObj);
-    }
-    
-    @Test(expected=RuntimeException.class)
+		assertJSONEquals(expected, retObj);
+	}
+
+	@Test
+	public void testFileOpen() throws Exception {
+		JSONObject retObj = clientRecorder.buildIDEFileEventJSON(ClientRecorder.EventType.fileOpen, "/workspace/package/filename.java");
+		JSONObject expected = new JSONObject();
+		expected.put("IDE", "IDEA");
+		expected.put("eventType", ClientRecorder.EventType.fileOpen.toString());
+		expected.put("fullyQualifiedMain", "/workspace/package/filename.java");
+		expected.put("timestamp", System.currentTimeMillis() / 1000);
+
+		assertJSONEquals(expected, retObj);
+	}
+
+	@Test
+	public void testFileClose() throws Exception {
+		JSONObject retObj = clientRecorder.buildIDEFileEventJSON(ClientRecorder.EventType.fileClose, "/workspace/package/filename.java");
+		JSONObject expected = new JSONObject();
+		expected.put("IDE", "IDEA");
+		expected.put("eventType", ClientRecorder.EventType.fileClose.toString());
+		expected.put("fullyQualifiedMain", "/workspace/package/filename.java");
+		expected.put("timestamp", System.currentTimeMillis() / 1000);
+
+		assertJSONEquals(expected, retObj);
+	}
+
+	@Test(expected = RuntimeException.class)
 	public void testTestRunNull() throws Exception {
 		clientRecorder.buildTestEventJSON(null, null);
 	}
-    
-    @Test(expected=RuntimeException.class)
+
+	@Test(expected = RuntimeException.class)
 	public void testTestRunEmpty() throws Exception {
 		clientRecorder.buildTestEventJSON("", "");
 	}
-    
-    @Test
+
+	@Test
 	public void testTestRun() throws Exception {
 		JSONObject actual = clientRecorder.buildTestEventJSON("/workspace/package/TestFoo/testBar", "success");
 		JSONObject expected = new JSONObject();
-		
+
 		expected.put("eventType", EventType.testRun.toString());
 		expected.put("IDE", clientRecorder.getIDE());
 		expected.put("fullyQualifiedTestMethod", "/workspace/package/TestFoo/testBar");
 		expected.put("testResult", "success");
-		
-		assertEquals(expected, actual);
+		expected.put("timestamp", System.currentTimeMillis() / 1000);
+
+		assertJSONEquals(expected, actual);
+	}
+
+	private void assertJSONEquals(JSONObject expected, JSONObject actual) {
+
+		assertEquals(expected.keySet(), actual.keySet());
+
+		for (Object key : expected.keySet()) {
+			if (key.equals("timestamp")) {
+				continue;
+			} else {
+				assertEquals(expected.get(key), actual.get(key));
+			}
+		}
 	}
 }
