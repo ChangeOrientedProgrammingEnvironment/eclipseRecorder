5e5a9b9af9de39ef9857cec51cc17267b0033742
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2013-11-13 11:23:04 -0800

    Started a simple test
    Added a test case and a small project to test my assumptions on.

---
+++ edu.oregonstate.cope.eclipse.tests/src/edu/oregonstate/cope/eclipse/OutsideLibrariesTest.java
@@ -0,0 +1,34 @@
+package edu.oregonstate.cope.eclipse;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.file.Paths;
+
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jdt.core.IJavaProject;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+
+public class OutsideLibrariesTest {
+	
+	@Rule public TestName name = new TestName();
+	private IJavaProject project;
+	
+	@Before
+	public void setUp() throws Exception {
+		InputStream stream = new FileInputStream(new File(Paths.get("projects/" + name.getMethodName() + "/.project").toAbsolutePath().toString()));
+		IProjectDescription projectDescription = ResourcesPlugin.getWorkspace().loadProjectDescription(stream);
+		
+	}
+	
+	@Test
+	public void testGetWorkspaceLibrary() {
+		
+	}
+
+}
