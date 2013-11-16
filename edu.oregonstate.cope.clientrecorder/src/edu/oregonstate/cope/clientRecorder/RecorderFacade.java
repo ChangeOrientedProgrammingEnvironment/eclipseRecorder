831899f36ab99b42f972bc615e0ab8b6301ecdb9
cdmihai <codobanmihai@gmail.com>
2013-11-15 18:32:11 -0800

    The RecorderFacade now exposes the uninstaller

--- edu.oregonstate.cope.clientrecorder/src/edu/oregonstate/cope/clientRecorder/RecorderFacade.java
+++ edu.oregonstate.cope.clientrecorder/src/edu/oregonstate/cope/clientRecorder/RecorderFacade.java
@@ -6,20 +6,36 @@ import edu.oregonstate.cope.clientRecorder.fileOps.EventFilesProvider;
 public class RecorderFacade {
 	private ClientRecorder clientRecorder;
 	private Properties properties;
+	private Uninstaller uninstaller;
 
 	public RecorderFacade initialize(String rootDirectory, String IDE) {
-		EventFilesProvider eventFileProvider = new EventFilesProvider();
-		eventFileProvider.setRootDirectory(rootDirectory);
-		ChangePersister.instance().setFileManager(eventFileProvider);
-		
+		initPersister(rootDirectory);
+		initProperties(rootDirectory);
+		initUninstaller();
+		initClientRecorder(IDE);
+
+		return this;
+	}
+
+	private void initClientRecorder(String IDE) {
+		clientRecorder = new ClientRecorder();
+		clientRecorder.setIDE(IDE);
+	}
+
+	private void initUninstaller() {
+		uninstaller = new Uninstaller(properties);
+	}
+
+	private void initProperties(String rootDirectory) {
 		ConfigFileProvider configFileProvider = new ConfigFileProvider();
 		configFileProvider.setRootDirectory(rootDirectory);
 		properties = new Properties(configFileProvider);
+	}
 
-		clientRecorder = new ClientRecorder();
-		clientRecorder.setIDE(IDE);
-		
-		return this;
+	private void initPersister(String rootDirectory) {
+		EventFilesProvider eventFileProvider = new EventFilesProvider();
+		eventFileProvider.setRootDirectory(rootDirectory);
+		ChangePersister.instance().setFileManager(eventFileProvider);
 	}
 
 	public ClientRecorder getClientRecorder() {
@@ -28,5 +44,9 @@ public class RecorderFacade {
 
 	public Properties getProperties() {
 		return properties;
+	}
+
+	public Uninstaller getUninstaller() {
+		return uninstaller;
 	}
 }
