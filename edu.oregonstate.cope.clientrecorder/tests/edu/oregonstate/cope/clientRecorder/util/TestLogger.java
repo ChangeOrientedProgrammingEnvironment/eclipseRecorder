911adbc16efd864a02f70bbae3077b1ac92cc144
cdmihai <codobanmihai@gmail.com>
2013-11-22 19:07:58 -0800

    added logger wrapper

---
+++ edu.oregonstate.cope.clientrecorder/tests/edu/oregonstate/cope/clientRecorder/util/TestLogger.java
@@ -0,0 +1,84 @@
+package edu.oregonstate.cope.clientRecorder.util;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestLogger {
+
+	private static final Path PARENT_DIR = Paths.get("testLogger").toAbsolutePath();
+	private COPELogger copeLogger;
+
+	@Before
+	public void setUp() {
+		copeLogger = new COPELogger();
+		copeLogger.enableFileLogging(PARENT_DIR.toAbsolutePath().toString());
+		copeLogger.disableConsoleLogging();
+	}
+
+	@After
+	public void tearDown() throws IOException {
+		copeLogger.testDisableFileLogging();
+
+		FileVisitor<Path> fv = new SimpleFileVisitor<Path>() {
+			@Override
+			public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+
+				Files.delete(file);
+
+				return FileVisitResult.CONTINUE;
+			}
+		};
+
+		Files.walkFileTree(PARENT_DIR, fv);
+		Files.delete(PARENT_DIR);
+	}
+
+	@Test
+	public void testLoggerWithError() throws Exception {
+		copeLogger.error(this, "oh no", new NullPointerException());
+		copeLogger.info(this, "test");
+		copeLogger.info(this, "test");
+		copeLogger.info(this, "test");
+
+		assertTrue(numberOfLoggedLines() > 4);
+	}
+
+	private int numberOfLoggedLines() throws IOException {
+		return Files.readAllLines(PARENT_DIR.resolve("log"), Charset.defaultCharset()).size();
+	}
+
+	@Test
+	public void testLogger() throws Exception {
+		copeLogger.info(this, "test");
+		copeLogger.info(this, "test");
+		copeLogger.info(this, "test");
+
+		assertEquals(3, numberOfLoggedLines());
+	}
+
+	@Test
+	public void testOnlyErrors() throws Exception {
+		copeLogger.logOnlyErrors();
+
+		copeLogger.info(this, "test");
+		copeLogger.info(this, "test");
+		copeLogger.error(this, "nooo");
+		copeLogger.info(this, "test");
+
+		assertEquals(1, numberOfLoggedLines());
+	}
+}
