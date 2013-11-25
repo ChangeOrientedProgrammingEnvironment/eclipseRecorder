1c7fd241455dab79befd4fc693324ab99ba50146
cdmihai <codobanmihai@gmail.com>
2013-11-24 19:22:16 -0800

    Bug fixes and refactorings
    - renamed paths
    - fix copy paste bugs
    - converted flags to boolean
    - extracted common string to constant
    - replaced file copy logic with Files.copy

--- edu.oregonstate.cope.eclipse.ui/src/edu/oregonstate/cope/eclipse/ui/SurveyManager.java
+++ edu.oregonstate.cope.eclipse.ui/src/edu/oregonstate/cope/eclipse/ui/SurveyManager.java
@@ -15,66 +15,62 @@ import edu.oregonstate.cope.eclipse.ui.handlers.SurveyWizard;
 public class SurveyManager {
 	
 
+	private static final String SAMPLE_SURVEY = "Question Number: 0 answer:-1 Question Number: 1 answer:-1";
+
 	public SurveyManager(){
 		
 	}
 	
 	public void checkAndRunSurvey(String WorkspaceDir,String BundleDir,Shell parent){
-		int workspaceFileExists = 0;
-		int bundleFileExists = 0;
-		Path workspacePath = Paths.get(WorkspaceDir,"survey.txt");
-		Path bundleDir = Paths.get(BundleDir,"survey.txt") ;
+		boolean workspaceFileExists = false;
+		boolean bundleFileExists = false;
+		Path workspaceSurveyPath = Paths.get(WorkspaceDir,"survey.txt");
+		Path bundleSurveyPath = Paths.get(BundleDir,"survey.txt") ;
 		
 		
-		if(Files.exists(workspacePath)){
-			workspaceFileExists = 1;
+		if(Files.exists(workspaceSurveyPath)){
+			workspaceFileExists = true;
 		}
-		if(Files.exists(bundleDir)){
-			workspaceFileExists = 1;
+		
+		if(Files.exists(bundleSurveyPath)){
+			bundleFileExists = true;
 		}
-		if(workspaceFileExists>0 && bundleFileExists>0){
+		
+		if(workspaceFileExists && bundleFileExists){
 			System.out.println("BOTH FILES EXIST");
-		}else if(workspaceFileExists<1 && bundleFileExists>0){
+		}else if(!workspaceFileExists && bundleFileExists){
 			
 			//String sampleSurvey = "Question Number: 0 answer:-1 Question Number: 1 answer:-1";
 			List<String> lines = null;
 			try {
-				 lines = Files.readAllLines(workspacePath, Charset.defaultCharset() );
-				 writeListToFile(Paths.get(WorkspaceDir,"survey.txt"),lines);
+				 Files.copy(bundleSurveyPath, workspaceSurveyPath);
 				 System.out.println("ONLY BUNDLE EXISTS");
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-					
 			
-		}else if(workspaceFileExists>0 && bundleFileExists<1){
+		}else if(workspaceFileExists && !bundleFileExists){
 			System.out.println("ONLY WORKSPACE EXISTS");
-			String sampleSurvey = "Question Number: 0 answer:-1 Question Number: 1 answer:-1";
-			writeContentsToFile(bundleDir,sampleSurvey);
-		}else if(workspaceFileExists<1 && bundleFileExists<1){
+			String sampleSurvey = SAMPLE_SURVEY;
+			writeContentsToFile(bundleSurveyPath,sampleSurvey);
+		}else if(!workspaceFileExists && !bundleFileExists){
 			System.out.println("NO FILE EXISTS, GIVE SURVEY");
 			
 //			DemoSurveyDialog dsd = new DemoSurveyDialog(parent);
 //			dsd.open();
 			
-
-			
-			
-			String sampleSurvey = "Question Number: 0 answer:-1 Question Number: 1 answer:-1";
-			writeContentsToFile(workspacePath,sampleSurvey);
-			writeContentsToFile(bundleDir,sampleSurvey);
+			String sampleSurvey = SAMPLE_SURVEY;
+			writeContentsToFile(workspaceSurveyPath,sampleSurvey);
+			writeContentsToFile(bundleSurveyPath,sampleSurvey);
 		}
 		
-		WizardDialog wizardDialog = new WizardDialog(parent.getShell(),
-			      new SurveyWizard());
-			    if (wizardDialog.open() == Window.OK) {
-			      System.out.println("Ok pressed");
-			    } else {
-			      System.out.println("Cancel pressed");
-			       }
-		
-		
+		WizardDialog wizardDialog = new WizardDialog(parent.getShell(), new SurveyWizard());
+		if (wizardDialog.open() == Window.OK) {
+			System.out.println("Ok pressed");
+		} else {
+			System.out.println("Cancel pressed");
+		}
 	}
 	
 	protected void writeContentsToFile(Path filePath, String currFileContents) {
@@ -84,12 +80,4 @@ public class SurveyManager {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
     }
-	protected void writeListToFile(Path filePath,List<String> contents){
-		try {
-            Files.write(filePath, contents,Charset.defaultCharset(),StandardOpenOption.CREATE);
-        } catch (IOException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-	}
-	
 }
