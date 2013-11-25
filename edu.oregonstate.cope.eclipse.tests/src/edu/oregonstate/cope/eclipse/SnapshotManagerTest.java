7620cafd78426a6e56b869dfdcbfdc1a5a3b3c4b
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2013-11-25 09:53:00 -0800

    Added a test to test the reading of known projects

--- edu.oregonstate.cope.eclipse.tests/src/edu/oregonstate/cope/eclipse/SnapshotManagerTest.java
+++ edu.oregonstate.cope.eclipse.tests/src/edu/oregonstate/cope/eclipse/SnapshotManagerTest.java
@@ -1,6 +1,11 @@
 package edu.oregonstate.cope.eclipse;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -10,7 +15,10 @@ public class SnapshotManagerTest {
 	private SnapshotManager snapshotManager;
 
 	@Before
-	public void setUp() {
+	public void setUp() throws Exception {
+		File file = new File(COPEPlugin.getLocalStorage(), "known-projects");
+		file.createNewFile();
+		Files.write(file.toPath(), "known1\nknown2\n".getBytes(), StandardOpenOption.WRITE);
 		snapshotManager = new SnapshotManager();
 	}
 
@@ -18,4 +26,11 @@ public class SnapshotManagerTest {
 	public void testNotKnowProject() {
 		assertFalse(snapshotManager.isProjectKnown("test"));
 	}
+	
+	@Test
+	public void testIsProjectKnown() {
+		assertTrue(snapshotManager.isProjectKnown("known1"));
+		assertTrue(snapshotManager.isProjectKnown("known2"));
+	}
+	
 }
