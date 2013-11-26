c07bff336e44db0e277ac98ed5694b890dd3aaac
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2013-11-26 12:11:31 -0800

    Saving the known projects

--- edu.oregonstate.cope.eclipse/src/edu/oregonstate/cope/eclipse/SnapshotManager.java
+++ edu.oregonstate.cope.eclipse/src/edu/oregonstate/cope/eclipse/SnapshotManager.java
@@ -8,6 +8,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
@@ -53,11 +54,11 @@ public class SnapshotManager {
 
 	protected void knowProject(String string) {
 		knownProjects.add(string);
-//		try {
-////			Files.write(Paths.get(parentDirectory, knownProjectsFileName), (string + "\n").getBytes(), StandardOpenOption.APPEND);
-//		} catch (IOException e) {
-//			COPEPlugin.getDefault().getLogger().error(this, e.getMessage(), e);
-//		}
+		try {
+			Files.write(Paths.get(parentDirectory, knownProjectsFileName), (string + "\n").getBytes(), StandardOpenOption.APPEND);
+		} catch (IOException e) {
+			COPEPlugin.getDefault().getLogger().error(this, e.getMessage(), e);
+		}
 	}
 	
 	private void knowProject(IProject project) {
