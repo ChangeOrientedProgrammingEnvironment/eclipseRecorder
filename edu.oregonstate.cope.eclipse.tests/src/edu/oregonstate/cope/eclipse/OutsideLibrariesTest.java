afbe5d9447d9fd278e64b1e188be3344df8e4a95
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2013-12-18 11:56:55 -0800

    Extracted superclass
    This is for tests that require an initialized workspace to work.
    
    The superclass initializes the workspace with one project.

--- edu.oregonstate.cope.eclipse.tests/src/edu/oregonstate/cope/eclipse/OutsideLibrariesTest.java
+++ edu.oregonstate.cope.eclipse.tests/src/edu/oregonstate/cope/eclipse/OutsideLibrariesTest.java
@@ -2,66 +2,24 @@ package edu.oregonstate.cope.eclipse;
 
 import static org.junit.Assert.*;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
+import org.junit.Rule;
 
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.core.runtime.Path;
-import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.JavaCore;
-import org.eclipse.jdt.internal.core.JavaProject;
-import org.eclipse.ui.dialogs.IOverwriteQuery;
-import org.eclipse.ui.wizards.datatransfer.FileSystemStructureProvider;
-import org.eclipse.ui.wizards.datatransfer.ImportOperation;
-import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestName;
 
-public class OutsideLibrariesTest {
+public class OutsideLibrariesTest extends PopulatedWorkspaceTest {
 
 	@Rule
 	public TestName name = new TestName();
-	private static IJavaProject javaProject;
-	private static SnapshotManager snapshotManager = new SnapshotManager(COPEPlugin.getLocalStorage().getAbsolutePath());
-
-	@BeforeClass
-	public static void setUp() throws Exception {
-		ImportOperation importOperation = new ImportOperation(new Path("librariesTest"), 
-				new File(Paths.get("projects" + File.separator + "librariesTest").toAbsolutePath().toString()), 
-				FileSystemStructureProvider.INSTANCE, 
-				new IOverwriteQuery() {
-
-			@Override
-			public String queryOverwrite(String pathString) {
-				return ALL;
-			}
-		});
-		importOperation.setCreateContainerStructure(false);
-		importOperation.run(new NullProgressMonitor());
-		IProject[] projects = ResourcesPlugin.getWorkspace().getRoot().getProjects();
-		if (projects.length > 1)
-			fail("More than 1 project in the workspace");
-
-		IProject project = projects[0];
-		project.open(new NullProgressMonitor());
-		String[] natureIds = project.getDescription().getNatureIds();
-		if (JavaProject.hasJavaNature(project)) {
-			javaProject = JavaCore.create(project);
-		} else {
-			fail("Project does not have java nature");
-		}
-	}
-
+	
 	@Test
 	public void testGetNonWorkspaceLibrary() throws Exception {
 		List<String> nonWorkspaceLibraries = snapshotManager.getNonWorkspaceLibraries(javaProject);
---
+++ edu.oregonstate.cope.eclipse.tests/src/edu/oregonstate/cope/eclipse/PopulatedWorkspaceTest.java
@@ -0,0 +1,57 @@
+package edu.oregonstate.cope.eclipse;
+
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.internal.core.JavaProject;
+import org.eclipse.ui.dialogs.IOverwriteQuery;
+import org.eclipse.ui.wizards.datatransfer.FileSystemStructureProvider;
+import org.eclipse.ui.wizards.datatransfer.ImportOperation;
+import org.junit.BeforeClass;
+
+public class PopulatedWorkspaceTest {
+
+	protected static IJavaProject javaProject;
+	protected static SnapshotManager snapshotManager = new SnapshotManager(COPEPlugin.getLocalStorage().getAbsolutePath());
+
+	@BeforeClass
+	public static void beforeClass() throws Exception {
+		ImportOperation importOperation = new ImportOperation(new Path("librariesTest"), 
+				new File(Paths.get("projects" + File.separator + "librariesTest").toAbsolutePath().toString()), 
+				FileSystemStructureProvider.INSTANCE, 
+				new IOverwriteQuery() {
+	
+			@Override
+			public String queryOverwrite(String pathString) {
+				return ALL;
+			}
+		});
+		importOperation.setCreateContainerStructure(false);
+		importOperation.run(new NullProgressMonitor());
+		IProject[] projects = ResourcesPlugin.getWorkspace().getRoot().getProjects();
+		if (projects.length > 1)
+			fail("More than 1 project in the workspace");
+	
+		IProject project = projects[0];
+		project.open(new NullProgressMonitor());
+		String[] natureIds = project.getDescription().getNatureIds();
+		if (JavaProject.hasJavaNature(project)) {
+			javaProject = JavaCore.create(project);
+		} else {
+			fail("Project does not have java nature");
+		}
+	}
+
+	public PopulatedWorkspaceTest() {
+		super();
+	}
+
+}
