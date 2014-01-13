119b45e3c9d5e7241125740ecbc42f4bc5e52223
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2014-01-13 10:58:34 -0800

    Added two buttons

--- edu.oregonstate.cope.eclipse.ui/src/edu/oregonstate/cope/eclipse/ui/handlers/ProjectSelectionPage.java
+++ edu.oregonstate.cope.eclipse.ui/src/edu/oregonstate/cope/eclipse/ui/handlers/ProjectSelectionPage.java
@@ -4,6 +4,7 @@ import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
@@ -21,8 +22,16 @@ public class ProjectSelectionPage extends Dialog {
 		composite.setLayout(new GridLayout(1, true));
 		composite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 		composite.setSize(400, 600);
-		Label label = new Label(composite, SWT.BORDER);
+		Label label = new Label(composite, SWT.BORDER);	
 		label.setText("Please select the project you would like us to record");
+		
+		Composite buttonsParent = new Composite(composite, SWT.NONE);
+		buttonsParent.setLayout(new GridLayout(2, false));
+		buttonsParent.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		Button selectAll = new Button(buttonsParent, SWT.NONE);
+		selectAll.setText("Select all");
+		Button selectNone = new Button(buttonsParent, SWT.NONE);
+		selectNone.setText("Select none");
 		return composite;
 	}
 }
