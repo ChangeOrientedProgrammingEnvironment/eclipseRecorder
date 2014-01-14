397a1df43b37a572ef044098a55841c33e52ebc4
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2014-01-14 10:47:47 -0800

    Made the InstallerOperation public

--- edu.oregonstate.cope.eclipse/src/edu/oregonstate/cope/eclipse/installer/InstallerOperation.java
+++ edu.oregonstate.cope.eclipse/src/edu/oregonstate/cope/eclipse/installer/InstallerOperation.java
@@ -6,7 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 
-abstract class InstallerOperation {
+public abstract class InstallerOperation {
 
 	protected Path workspaceDirectory;
 	protected Path permanentDirectory;
