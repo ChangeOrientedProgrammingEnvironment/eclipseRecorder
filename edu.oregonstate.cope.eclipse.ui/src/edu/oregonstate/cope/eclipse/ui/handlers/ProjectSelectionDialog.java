18b9bb405808be13c4a0dbdee0b59ae69aef88b2
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2014-01-14 17:07:13 -0800

    Import reorg

--- edu.oregonstate.cope.eclipse.ui/src/edu/oregonstate/cope/eclipse/ui/handlers/ProjectSelectionDialog.java
+++ edu.oregonstate.cope.eclipse.ui/src/edu/oregonstate/cope/eclipse/ui/handlers/ProjectSelectionDialog.java
@@ -7,7 +7,6 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.TableItem;
 
 import edu.oregonstate.cope.eclipse.ui.ProjectSelectionComposite;
 
