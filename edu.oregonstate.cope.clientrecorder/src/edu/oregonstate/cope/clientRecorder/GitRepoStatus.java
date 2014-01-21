bc94adef34fc7f340bf7d0cee608885cec1b2d11
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2014-01-20 18:50:03 -0800

    Populating the fields
    And correctly building the JSON

--- edu.oregonstate.cope.clientrecorder/src/edu/oregonstate/cope/clientRecorder/GitRepoStatus.java
+++ edu.oregonstate.cope.clientrecorder/src/edu/oregonstate/cope/clientRecorder/GitRepoStatus.java
@@ -1,5 +1,6 @@
 package edu.oregonstate.cope.clientRecorder;
 
+import java.util.ArrayList;
 import java.util.Set;
 
 import org.json.simple.JSONObject;
@@ -15,6 +16,9 @@ public class GitRepoStatus {
 	public GitRepoStatus(String branch, String commitSHA1, Set<String> filesModified, Set<String> filesAdded, Set<String> filesDeleted) {
 		this.branch = branch;
 		this.commitSHA1 = commitSHA1;
+		this.filesModified = filesModified;
+		this.filesAdded = filesAdded;
+		this.filesDeleted = filesDeleted;
 	}
 	
 	public String getBranch() {
@@ -29,9 +33,9 @@ public class GitRepoStatus {
 		JSONObject jsonObject = new JSONObject();
 		jsonObject.put(JSONConstants.JSON_GIT_HEAD_COMMIT, commitSHA1);
 		jsonObject.put(JSONConstants.JSON_GIT_BRANCH, branch);
-		jsonObject.put(JSONConstants.JSON_GIT_FILES_MODIFIED, filesModified);
-		jsonObject.put(JSONConstants.JSON_GIT_FILES_ADDED, filesAdded);
-		jsonObject.put(JSONConstants.JSON_GIT_FILES_REMOVED, filesDeleted);
+		jsonObject.put(JSONConstants.JSON_GIT_FILES_MODIFIED, new ArrayList<String>(filesModified));
+		jsonObject.put(JSONConstants.JSON_GIT_FILES_ADDED, new ArrayList<String>(filesAdded));
+		jsonObject.put(JSONConstants.JSON_GIT_FILES_REMOVED, new ArrayList<String>(filesDeleted));
 		return jsonObject;
 	}
 
