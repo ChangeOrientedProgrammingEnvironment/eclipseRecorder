ed38a2d6de8d421b4596af5f358137cbdb9176ad
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2014-01-27 16:22:12 -0800

    Created a mock recorder

---
+++ edu.oregonstate.cope.eclipse.tests/src/edu/oregonstate/cope/eclipse/MockRecorder.java
@@ -0,0 +1,22 @@
+package edu.oregonstate.cope.eclipse;
+
+import edu.oregonstate.cope.clientRecorder.ClientRecorder;
+
+public class MockRecorder extends ClientRecorder {
+	
+	public String recordedText = "bob";
+	public int recordedOffset = 1000000;
+	public int recordedLength = 10000000;
+	public String recordedSourceFile = "nothing";
+	public String recordedChangeOrigin = "somebody else";
+
+	@Override
+	public void recordTextChange(String text, int offset, int length, String sourceFile, String changeOrigin) {
+		System.out.println(text);
+		recordedText = text;
+		recordedOffset = offset;
+		recordedLength = length;
+		recordedSourceFile = sourceFile;
+		recordedChangeOrigin = changeOrigin;
+	}
+}
