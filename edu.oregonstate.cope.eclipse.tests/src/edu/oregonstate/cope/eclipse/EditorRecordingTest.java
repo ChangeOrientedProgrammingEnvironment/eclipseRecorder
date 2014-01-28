44e2d268d1b64b06082ad70665cd807a38b709b9
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2014-01-28 15:29:04 -0800

    SWTBot stuff and testing the UNDO!
    Hurray, it works!

--- edu.oregonstate.cope.eclipse.tests/src/edu/oregonstate/cope/eclipse/EditorRecordingTest.java
+++ edu.oregonstate.cope.eclipse.tests/src/edu/oregonstate/cope/eclipse/EditorRecordingTest.java
@@ -4,33 +4,54 @@ import static org.junit.Assert.assertEquals;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jdt.ui.JavaUI;
+import org.eclipse.jface.bindings.keys.KeyStroke;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.ITextOperationTarget;
 import org.eclipse.jface.text.source.ISourceViewer;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotEclipseEditor;
+import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.eclipse.swtbot.swt.finder.keyboard.Keyboard;
+import org.eclipse.swtbot.swt.finder.keyboard.KeyboardFactory;
+import org.eclipse.swtbot.swt.finder.keyboard.Keystrokes;
+import org.eclipse.swtbot.swt.finder.keyboard.SWTKeyboardStrategy;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchCommandConstants;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.commands.ICommandService;
 import org.eclipse.ui.handlers.IHandlerService;
 import org.eclipse.ui.part.FileEditorInput;
 import org.eclipse.ui.part.MultiPageEditorPart;
 import org.eclipse.ui.tests.harness.util.FileUtil;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import edu.oregonstate.cope.clientRecorder.ChangeOrigin;
 import edu.oregonstate.cope.eclipse.listeners.DocumentListener;
 
+@RunWith(SWTBotJunit4ClassRunner.class)
 public class EditorRecordingTest {
 
 	private MockRecorder recorder;
 	private IDocument document;
 	private IEditorPart editor;
+	private static SWTWorkbenchBot bot;
+	
 	private boolean done;
+	
+	@BeforeClass
+	public static void beforeClass() {
+		bot = new SWTWorkbenchBot();
+		bot.viewByTitle("Welcome").close();
+	}
 
 	@Before
 	public void before() throws Exception {
@@ -83,14 +104,24 @@ public class EditorRecordingTest {
 	
 	@Test
 	public void testUndo() throws Exception {
-		document.replace(0, 1, "!");
-
-		PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().activate(editor);
+		assertEquals(editor, bot.activeEditor().getReference().getEditor(true));
+		SWTBotEclipseEditor textEditor = bot.activeEditor().toTextEditor();
+		String text = textEditor.getText();
+		System.out.println(text);
+		textEditor.typeText(0, 0, "H");
+		text = textEditor.getText();
+		System.out.println(text);
 		
-//		ICommandService commandService = (ICommandService) PlatformUI.getWorkbench().getAdapter(ICommandService.class);
-		IHandlerService handlerService = (IHandlerService) editor.getSite().getService(IHandlerService.class);
-		handlerService.executeCommand(IWorkbenchCommandConstants.EDIT_UNDO, null);
+		KeyStroke command = KeyStroke.getInstance(SWT.COMMAND, KeyStroke.NO_KEY);
+		KeyStroke z = KeyStroke.getInstance("Z");
+
+		KeyboardFactory.getSWTKeyboard().pressShortcut(command, z);
+		
+		Thread.sleep(100);
 		
 		assertEquals(ChangeOrigin.UNDO, recorder.recordedChangeOrigin);
+		assertEquals(1,recorder.recordedLength);
+		assertEquals(0,recorder.recordedOffset);
+		assertEquals("",recorder.recordedText);
 	}
 }
