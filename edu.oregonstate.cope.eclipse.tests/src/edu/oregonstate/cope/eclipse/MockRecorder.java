c60379052e34206eb9222c898b72c5dd3721cb86
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2014-01-28 16:02:22 -0800

    Added recording for Copy

--- edu.oregonstate.cope.eclipse.tests/src/edu/oregonstate/cope/eclipse/MockRecorder.java
+++ edu.oregonstate.cope.eclipse.tests/src/edu/oregonstate/cope/eclipse/MockRecorder.java
@@ -1,9 +1,11 @@
 package edu.oregonstate.cope.eclipse;
 
 import edu.oregonstate.cope.clientRecorder.ClientRecorder;
+import edu.oregonstate.cope.clientRecorder.Events;
 
 public class MockRecorder extends ClientRecorder {
 	
+	public Events recordedEvent;
 	public String recordedText = "bob";
 	public int recordedOffset = 1000000;
 	public int recordedLength = 10000000;
@@ -12,10 +14,20 @@ public class MockRecorder extends ClientRecorder {
 
 	@Override
 	public void recordTextChange(String text, int offset, int length, String sourceFile, String changeOrigin) {
+		recordedEvent = Events.textChange;
 		recordedText = text;
 		recordedOffset = offset;
 		recordedLength = length;
 		recordedSourceFile = sourceFile;
 		recordedChangeOrigin = changeOrigin;
+	}
+
+	@Override
+	public void recordCopy(String entityAddress, int offset, int lenght, String copiedText) {
+		recordedEvent = Events.copy;
+		recordedSourceFile = entityAddress;
+		recordedOffset = offset;
+		recordedLength = lenght;
+		recordedText = copiedText;
 	}
 }
