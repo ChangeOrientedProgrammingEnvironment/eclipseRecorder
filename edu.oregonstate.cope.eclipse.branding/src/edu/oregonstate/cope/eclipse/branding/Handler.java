c15ccc4c1dd45d812e5449c7a85dec57dd437c20
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2014-02-10 14:42:28 -0800

    The handler calles the command

--- edu.oregonstate.cope.eclipse.branding/src/edu/oregonstate/cope/eclipse/branding/Handler.java
+++ edu.oregonstate.cope.eclipse.branding/src/edu/oregonstate/cope/eclipse/branding/Handler.java
@@ -4,16 +4,35 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.Command;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.commands.IHandler;
-import org.eclipse.core.commands.IHandlerListener;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.swt.SWT;
+import org.eclipse.core.commands.NotEnabledException;
+import org.eclipse.core.commands.NotHandledException;
+import org.eclipse.core.commands.common.NotDefinedException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.commands.ICommandService;
+import org.eclipse.ui.handlers.IHandlerService;
 
 public class Handler extends AbstractHandler {
 	
+	private void runCommand(String commandId) {
+		ICommandService commandService= (ICommandService)PlatformUI.getWorkbench().getService(ICommandService.class);
+		Command command= commandService.getCommand(commandId);
+		if (!command.isDefined()) {
+			return;
+		}
+		IHandlerService handlerService= (IHandlerService)PlatformUI.getWorkbench().getService(IHandlerService.class);
+		try {
+			handlerService.executeCommand(commandId, null);
+		} catch (ExecutionException e) {
+		} catch (NotDefinedException e) {
+		} catch (NotEnabledException e) {
+		} catch (NotHandledException e) {
+		}
+	}
+	
 	@Override
 	public Object execute(ExecutionEvent event) {
-		LogoManager.getInstance().showUpdateIsAvailable();
+		String commandID = LogoManager.getInstance().getCommandToExecute();
+		runCommand(commandID);
 		return null;
 	}
 
