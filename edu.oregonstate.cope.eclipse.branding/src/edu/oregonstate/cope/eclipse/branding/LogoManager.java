55eae87df087696f49f6f4693c91d47ea1e31366
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2014-02-10 09:34:42 -0800

    Added the code from CodingTracker
    And made it more human

--- edu.oregonstate.cope.eclipse.branding/src/edu/oregonstate/cope/eclipse/branding/LogoManager.java
+++ edu.oregonstate.cope.eclipse.branding/src/edu/oregonstate/cope/eclipse/branding/LogoManager.java
@@ -1,16 +1,59 @@
 package edu.oregonstate.cope.eclipse.branding;
 
+import org.eclipse.core.runtime.Assert;
+import org.eclipse.core.runtime.AssertionFailedException;
+import org.eclipse.jface.action.IStatusLineManager;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.ui.IStartup;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.internal.WorkbenchWindow;
+import org.eclipse.ui.texteditor.StatusLineContributionItem;
 
+@SuppressWarnings("restriction")
 public class LogoManager {
+	
+	private final static String STATUS_LINE_CONTRIBUTION_ITEM_ID= "edu.illinois.codingspectator.branding.StatusLine";
+
+	private IStatusLineManager statusLineManager;
+	
+	private IStatusLineManager getStatusLineManager() {
+		if (statusLineManager != null)
+			return statusLineManager;
+		IWorkbenchWindow activeWindow= PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+		if (activeWindow == null)
+			return null;
+		
+		statusLineManager = ((WorkbenchWindow)activeWindow).getStatusLineManager();
+		
+		return statusLineManager;
+	}
+
+	private boolean logoExists() {
+		return getStatusLineManager().find(STATUS_LINE_CONTRIBUTION_ITEM_ID) != null;
+	}
+
+	private void addLogoToStatusLine() {
+		Image codingspectatorLogo= Activator.imageDescriptorFromPlugin(Activator.PLUGIN_ID, "icons/black on transparent.png").createImage(); //$NON-NLS-1$
+		StatusLineContributionItem contributionItem= new StatusLineContributionItem(STATUS_LINE_CONTRIBUTION_ITEM_ID);
+		contributionItem.setImage(codingspectatorLogo);
+		contributionItem.setToolTipText("COPE recorder");
+		getStatusLineManager().add(contributionItem);
+		getStatusLineManager().update(false);
+
+		try {
+			Assert.isTrue(logoExists());
+		} catch (AssertionFailedException e) {
+			// add loggin stuff
+		}
+	}
 
 	public void showLogo() {
 		Display.getDefault().asyncExec(new Runnable() {
 
 			@Override
 			public void run() {
-				//Activator.getDefault().getStatusLineUpdater().updateStatusLine();
+				addLogoToStatusLine();
 			}
 		});
 	}
