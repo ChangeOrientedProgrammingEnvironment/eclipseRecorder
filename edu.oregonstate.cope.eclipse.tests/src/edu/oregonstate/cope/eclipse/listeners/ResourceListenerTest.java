e66026d04da72c5f617f44d27fdc03b7d8e2b3cb
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2014-02-18 09:52:39 -0800

    Added test for dumb text file

--- edu.oregonstate.cope.eclipse.tests/src/edu/oregonstate/cope/eclipse/listeners/ResourceListenerTest.java
+++ edu.oregonstate.cope.eclipse.tests/src/edu/oregonstate/cope/eclipse/listeners/ResourceListenerTest.java
@@ -1,5 +1,6 @@
 package edu.oregonstate.cope.eclipse.listeners;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.ByteArrayInputStream;
@@ -9,6 +10,7 @@ import java.util.Random;
 import org.apache.commons.codec.binary.Base64;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.junit.After;
 import org.junit.Before;
@@ -20,16 +22,31 @@ public class ResourceListenerTest {
 	
 	private static final int FILE_SIZE_BYTES = 10000;
 	private IProject project;
-	private IFile file;
+	private IFile binaryFile;
 	private byte[] randomBytes;
+	private IFile textFile;
+	private String textFileContents;
+	private ResourceListener listener;
 
 	@Before
 	public void before() throws Exception {
 		project = FileUtil.createProject("TestProject");
-		file = FileUtil.createFile("bla.jar", project);
+		createBinaryFile();
+		createTextFile();
+		listener = new ResourceListener();
+	}
+
+	private void createTextFile() throws Exception {
+		textFile = FileUtil.createFile("smth.txt", project);
+		textFileContents = "Some human readable text";
+		textFile.appendContents(new ByteArrayInputStream(textFileContents.getBytes()), true, false, new NullProgressMonitor());
+	}
+
+	private void createBinaryFile() throws CoreException {
+		binaryFile = FileUtil.createFile("bla.jar", project);
 		randomBytes = new byte[FILE_SIZE_BYTES];
 		new Random().nextBytes(randomBytes);
-		file.appendContents(new ByteArrayInputStream(randomBytes), true, false, new NullProgressMonitor());
+		binaryFile.appendContents(new ByteArrayInputStream(randomBytes), true, false, new NullProgressMonitor());
 	}
 	
 	@After
@@ -39,12 +56,17 @@ public class ResourceListenerTest {
 	
 	@Test
 	public void testReadBinaryFile() throws Exception {
-		ResourceListener listener = new ResourceListener();
-		String contents = listener.getFileContents(file);
+		String contents = listener.getFileContents(binaryFile);
 		
 		byte[] actualBytes = Base64.decodeBase64(contents.getBytes());
 		
 		assertTrue(Arrays.equals(randomBytes, actualBytes));
+	}
+	
+	@Test
+	public void testReadTextFile() throws Exception {
+		String fileContents = listener.getFileContents(textFile);
+		assertEquals(textFileContents, fileContents);
 	}
 
 }
