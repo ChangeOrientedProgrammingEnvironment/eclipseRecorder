29360f47feca77204967f2b9a7e9f154ab28a997
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2014-03-04 18:47:02 -0800

    Moved the main infra into the submodule
    Now we can reuse just this, instead of the whole shebang

--- edu.oregonstate.cope.clientrecorder/README.md
+++
@@ -1,4 +0,0 @@
-clientRecorder
-==============
-
-This is the code that will record the clients edits for the COPE studies
--- edu.oregonstate.cope.clientrecorder/src/edu/oregonstate/cope/clientRecorder/RecordException.java
+++
@@ -1,9 +0,0 @@
-package edu.oregonstate.cope.clientRecorder;
-
-public class RecordException extends Exception {
-
-	public RecordException(String message) {
-		super(message);
-	}
-
-}
