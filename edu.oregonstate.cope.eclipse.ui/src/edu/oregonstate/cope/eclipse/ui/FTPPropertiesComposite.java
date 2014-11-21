8baadd92c08879ee2266f8b7d0250a2bb1de9f55
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2014-11-20 16:30:49 -0800

    Added UI elements for the upload limit part

--- edu.oregonstate.cope.eclipse.ui/src/edu/oregonstate/cope/eclipse/ui/FTPPropertiesComposite.java
+++ edu.oregonstate.cope.eclipse.ui/src/edu/oregonstate/cope/eclipse/ui/FTPPropertiesComposite.java
@@ -4,11 +4,16 @@ import java.io.IOException;
 import java.security.GeneralSecurityException;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Text;
 
 import edu.oregonstate.cope.clientRecorder.Properties;
@@ -21,6 +26,8 @@ public class FTPPropertiesComposite extends Composite {
 	private Text port;
 	private Text username;
 	private Text password;
+	private Text limitText;
+	private Button limitButton;
 
 	public FTPPropertiesComposite(Composite parent, int style) {
 		super(parent, style);
@@ -28,7 +35,16 @@ public class FTPPropertiesComposite extends Composite {
 		Composite composite = this;
 		composite.setLayout(new GridLayout(1, true));
 		composite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		
+		Properties workspaceProperties = COPEPlugin.getDefault()
+				.getWorkspaceProperties();
 
+		populateFTPOptions(composite, workspaceProperties);
+		populateUploadSettings(composite, workspaceProperties);
+	}
+
+	private void populateFTPOptions(Composite composite,
+			Properties workspaceProperties) {
 		Group sftpGroup = new Group(composite, SWT.SHADOW_ETCHED_IN);
 		sftpGroup.setLayout(new GridLayout(4, false));
 		sftpGroup.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
@@ -49,8 +65,6 @@ public class FTPPropertiesComposite extends Composite {
 		password = new Text(sftpGroup, SWT.PASSWORD | SWT.BORDER);
 
 		// Getting stored FTP properties
-		Properties workspaceProperties = COPEPlugin.getDefault()
-				.getWorkspaceProperties();
 		FTPConnectionProperties ftpProperties = new FTPConnectionProperties();
 		String preferencesHostname = workspaceProperties.getProperty(
 				COPEPlugin.PREFERENCES_HOSTNAME, "");
@@ -80,6 +94,46 @@ public class FTPPropertiesComposite extends Composite {
 		sftpGroup.pack();
 	}
 
+	private void populateUploadSettings(Composite composite,
+			Properties workspaceProperties) {
+		Group uploadSettings = new Group(composite, SWT.SHADOW_ETCHED_IN);
+		uploadSettings.setLayout(new GridLayout(2, false));
+		uploadSettings.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		uploadSettings.setText("Upload options");
+		Label limitLabel = new Label(uploadSettings, SWT.NONE);
+		limitLabel.setText("Limit Upload");
+		limitButton = new Button(uploadSettings, SWT.CHECK);
+		Label speedLabel = new Label(uploadSettings, SWT.NONE);
+		speedLabel.setText("Limit rate (KB/s):");
+		limitText = new Text(uploadSettings, SWT.BORDER);
+		limitText.setEnabled(false);
+		limitText.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
+		
+		String shouldUploadString = workspaceProperties.getProperty(COPEPlugin.PREFERENCES_SHOULD_LIMIT_UPLOAD, "false");
+		boolean shouldUpload = Boolean.parseBoolean(shouldUploadString);
+		if (shouldUpload) {
+			limitButton.setSelection(true);
+			limitText.setEnabled(true);
+		}
+		
+		String limitRate = workspaceProperties.getProperty(COPEPlugin.PREFERENCES_UPLOAD_LIMIT, "200");
+		limitText.setText(limitRate);
+		
+		limitButton.addSelectionListener(new SelectionListener() {
+
+			public void widgetSelected(SelectionEvent event) {
+				limitText.setEnabled(limitButton.getSelection());
+			}
+
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+			}
+			
+		});
+		
+		uploadSettings.pack();
+	}
+
 	public String getHostname() {
 		return hostname.getText();
 	}
@@ -97,10 +151,14 @@ public class FTPPropertiesComposite extends Composite {
 	}
 
 	public boolean shouldLimitBandwith() {
-		return true;
+		return limitButton.getSelection();
 	}
 
-	public int getLimitInKBs() {
-		return 300;
+	public int getUploadLimit() throws NumberFormatException {
+		try {
+			return Integer.parseInt(limitText.getText());
+		} catch (NumberFormatException e) {
+			throw e;
+		}
 	}
 }
