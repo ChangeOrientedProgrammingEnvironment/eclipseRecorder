441d88bd01f7055df7d66e0b00eef7616ea0c788
Nicholas Nelson <nelsonni@oregonstate.edu>
2015-09-28 13:45:34 -0700

    Updated updatesite shell script to signify eventual deprecation and removal of email notification

--- edu.oregonstate.cope.eclipse.updatesite/upload.sh
+++ edu.oregonstate.cope.eclipse.updatesite/upload.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# This is an old obsolete command that will be deprecated in future releases.
 site_location='brindesc@access.engr.oregonstate.edu:~/public_html/cope/client-recorder/'
 
 rsync -avz . $site_location
